916f811fee9369b29f734716e0e99b3da1f151c8
tinuola <24995224+tinuola@users.noreply.github.com>
2023-05-05 20:57:13 -0700

    Change tip list to input tags

--- js/app.js
+++ js/app.js
@@ -11,150 +11,145 @@ Currently displays NaN
 - Add this to calculate function
 */
 
-const billInput = document.querySelector('#bill-input')
-const billErrorMsg = document.querySelector('.bill-input-msg')
-let billValue 
+const billInput = document.querySelector('#bill-input');
+const billErrorMsg = document.querySelector('.bill-input-msg');
+let billValue;
 
-const peopleInput = document.querySelector('#people-input')
-const peopleErrorMsg = document.querySelector('.people-input-msg')
-let peopleValue
+const peopleInput = document.querySelector('#people-input');
+const peopleErrorMsg = document.querySelector('.people-input-msg');
+let peopleValue;
 
-const tipPercents = document.querySelectorAll('.tip-percent')
-const customTipInput = document.querySelector('#custom-tip-input')
-const tipErrorMsg = document.querySelector('.tip-input-msg')
-let customTipValue
+const tipPercents = document.querySelectorAll('.tip-percent');
+const customTipInput = document.querySelector('#custom-tip-input');
+const tipErrorMsg = document.querySelector('.tip-input-msg');
+let customTipValue;
 
-const tipAmtDisplay = document.querySelector('#tip-amount')
-const totalAmtDisplay = document.querySelector('#total-amount')
+const tipAmtDisplay = document.querySelector('#tip-amount');
+const totalAmtDisplay = document.querySelector('#total-amount');
 
-const resetBtn = document.querySelector('#reset-btn')
+const resetBtn = document.querySelector('#reset-btn');
 
-const floatRegex = /^[+]?(?=.)(?:\d+,)*\d*(?:\.\d+)?$/
-const intRegex = /^\d*$/
+const floatRegex = /^[+]?(?=.)(?:\d+,)*\d*(?:\.\d+)?$/;
+const intRegex = /^\d*$/;
 
-const tips = [5, 10, 15, 20, 25]
+const tips = [5, 10, 15, 20, 25];
 
-let currentTip // Store/track current tip value
-
+let currentTip; // Store/track current tip value
 
 /******************
 FUNCTIONS 
 *******************/
 
-
 // Display percentages from tips array
 const displayTipValues = () => {
-  tipPercents.forEach((tip,i) => {
-    tip.innerHTML = `${tips[i]}%`
-  })
-}
-displayTipValues()
-
+  tipPercents.forEach((tip, i) => {
+    // tip.innerHTML = `${tips[i]}%`
+    tip.value = `${tips[i]}`;
+  });
+};
+displayTipValues();
 
 // Verify if input field for bill or people is empty
 // Used by tip event listeners
 const verifyNonEmptyInput = () => {
-  billValue = billInput.value
-  peopleValue = peopleInput.value
-  return (billValue === '' || peopleValue === '')
-}
-
+  billValue = billInput.value;
+  peopleValue = peopleInput.value;
+  return billValue === '' || peopleValue === '';
+};
 
 // Calculate values and display final result
 const calcShowResults = (num) => {
-  let isInputEmpty = verifyNonEmptyInput()
+  let isInputEmpty = verifyNonEmptyInput();
 
-  if(isInputEmpty){
-    tipErrorMsg.innerHTML = `Please enter Bill amount and Number of People.`
+  if (isInputEmpty) {
+    tipErrorMsg.innerHTML = `Please enter Bill amount and Number of People.`;
     return null;
   } else {
-    billValue = Number(billInput.value)
-    peopleValue = Number(peopleInput.value)
+    billValue = Number(billInput.value);
+    peopleValue = Number(peopleInput.value);
 
-    let tip = num/100
-    let tipAmt = billValue * tip
-    let totalAmt = billValue + tipAmt
+    let tip = num / 100;
+    let tipAmt = billValue * tip;
+    let totalAmt = billValue + tipAmt;
 
-    tipAmtDisplay.innerHTML = `$${(tipAmt / peopleValue).toFixed(2)}`
-    totalAmtDisplay.innerHTML = `$${(totalAmt / peopleValue).toFixed(2)}`
+    tipAmtDisplay.innerHTML = `$${(tipAmt / peopleValue).toFixed(2)}`;
+    totalAmtDisplay.innerHTML = `$${(totalAmt / peopleValue).toFixed(2)}`;
 
-    tipErrorMsg.innerHTML = ``
+    tipErrorMsg.innerHTML = ``;
 
-    resetBtn.style.backgroundColor = 'green'
+    resetBtn.style.backgroundColor = 'green';
   }
-}
-
+};
 
 // Reset calculator
-const reset = () =>{
-  billInput.value = ''
-  peopleInput.value = ''
-  customTipInput.value = ''
-  tipAmtDisplay.innerHTML = `$0.00`
-  totalAmtDisplay.innerHTML = `$0.00`
-  resetBtn.style.backgroundColor = 'transparent'
-}
-
+const reset = () => {
+  billInput.value = '';
+  peopleInput.value = '';
+  customTipInput.value = '';
+  tipAmtDisplay.innerHTML = `$0.00`;
+  totalAmtDisplay.innerHTML = `$0.00`;
+  resetBtn.style.backgroundColor = 'transparent';
+};
 
 /******************
 EVENT LISTENERS 
 *******************/
 
-
 // Validate input field for bill
-billInput.addEventListener('change', function(){
-  billValue = Number(this.value)
-  let isNum = floatRegex.test(billValue)
+billInput.addEventListener('change', function () {
+  billValue = Number(this.value);
+  let isNum = floatRegex.test(billValue);
 
-  !isNum ? 
-  billErrorMsg.innerHTML = `Please enter a valid number.` 
-  : billValue === 0 ? billErrorMsg.innerHTML = `Cannot be a zero` 
-  : billErrorMsg.innerHTML = ``
+  !isNum
+    ? (billErrorMsg.innerHTML = `Please enter a valid number.`)
+    : billValue === 0
+    ? (billErrorMsg.innerHTML = `Cannot be zero`)
+    : (billErrorMsg.innerHTML = ``);
 
-  calcShowResults(currentTip) 
-})
-
+  calcShowResults(currentTip);
+});
 
 // Validate input field for people
-peopleInput.addEventListener('change', function(){
-  peopleValue = Number(this.value)
-  let isNum = intRegex.test(peopleValue)
+peopleInput.addEventListener('change', function () {
+  peopleValue = Number(this.value);
+  let isNum = intRegex.test(peopleValue);
 
-  !isNum ? peopleErrorMsg.innerHTML = `Please enter a valid number.`  
-  : peopleValue === 0 ? peopleErrorMsg.innerHTML = `Cannot be a zero` 
-  : peopleErrorMsg.innerHTML = ``
+  !isNum
+    ? (peopleErrorMsg.innerHTML = `Please enter a valid number.`)
+    : peopleValue === 0
+    ? (peopleErrorMsg.innerHTML = `Cannot be a zero`)
+    : (peopleErrorMsg.innerHTML = ``);
 
-  calcShowResults(currentTip)
-})
-
+  calcShowResults(currentTip);
+});
 
 tipPercents.forEach((tip, i) => {
-  tip.addEventListener('click', function(){
-    let isInputEmpty = verifyNonEmptyInput()
-    currentTip = tips[i]
-    tip.style.color = 'tomato'
+  tip.addEventListener('click', function () {
+    let isInputEmpty = verifyNonEmptyInput();
+    currentTip = tips[i];
+    tip.style.color = 'tomato';
 
-    isInputEmpty ? 
-    tipErrorMsg.innerHTML = `Bill or Number of People cannot be empty` :
-    calcShowResults(tips[i])
+    isInputEmpty
+      ? (tipErrorMsg.innerHTML = `Bill or Number of People cannot be empty`)
+      : calcShowResults(tips[i]);
 
-    customTipInput.value = ``
-  })
-})
+    customTipInput.value = ``;
+  });
+});
 
+customTipInput.addEventListener('change', function () {
+  customTipValue = Number(this.value);
+  let isNum = intRegex.test(customTipValue);
 
-customTipInput.addEventListener('change', function(){
-  customTipValue = Number(this.value)
-  let isNum = intRegex.test(customTipValue)
+  let isInputEmpty = verifyNonEmptyInput();
+  currentTip = customTipValue;
+  customTipInput.style.color = 'tomato';
 
-  let isInputEmpty = verifyNonEmptyInput()
-  currentTip = customTipValue
-  customTipInput.style.color = 'tomato'
+  !isNum
+    ? (tipErrorMsg.innerHTML = `Please enter a valid number.`)
+    : isInputEmpty
+    ? (tipErrorMsg.innerHTML = `Bill or Number of People cannot be empty`)
+    : calcShowResults(customTipValue);
+});
 
-  !isNum ? tipErrorMsg.innerHTML = `Please enter a valid number.` 
-  : isInputEmpty ? tipErrorMsg.innerHTML = `Bill or Number of People cannot be empty`
-  : calcShowResults(customTipValue)
-})
-
-
-resetBtn.addEventListener('click', reset)
+resetBtn.addEventListener('click', reset);
