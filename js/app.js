24bf59de59a93162b7530bf0ba2dfd7a735ca34a
tinuola <24995224+tinuola@users.noreply.github.com>
2023-05-14 21:50:36 -0700

    Organize code

--- js/app.js
+++ js/app.js
@@ -2,17 +2,21 @@ let billValue;
 let peopleValue;
 let currentTip;
 
+const billInput = document.querySelector('#bill-input');
+const peopleInput = document.querySelector('#people-input');
+
+const tipPercents = document.querySelectorAll('.tip-percent');
 const customTipInput = document.querySelector('#custom-tip');
 
 const errorMsgs = document.querySelectorAll('.display-error p');
 
 const tipAmtDisplay = document.querySelector('#tip-amount-display');
-
 const totalAmtDisplay = document.querySelector('#total-amount-display');
 
 const resetBtn = document.querySelector('#reset-btn');
 
 const floatRegex = /^[+]?(?=.)(?:\d+,)*\d*(?:\.\d+)?$/;
+const intRegex = /^\d*$/;
 
 const errorMsgsList = {
   1: 'Enter a valid number',
@@ -23,20 +27,6 @@ const errorMsgsList = {
   6: 'Enter bill amount and number of people',
   7: "Select a tip; don't be cheap!",
 };
-
-// Old variables to refactor or delete
-const billInput = document.querySelector('#bill-input');
-// let billValue;
-
-const peopleInput = document.querySelector('#people-input');
-// let peopleValue;
-
-const tipPercents = document.querySelectorAll('.tip-percent');
-// const customTipInput = document.querySelector('#custom-tip');
-const tipErrorMsg = document.querySelector('.tip-input-msg');
-// let customTipValue;
-
-const intRegex = /^\d*$/;
 
 /******************
 EVENT LISTENERS 
@@ -49,7 +39,7 @@ document.addEventListener('input', function (e) {
     let inputIndex = input.getAttribute('data-input');
     let errorMsg = errorMsgs[inputIndex];
     let isNum = floatRegex.test(inputValue);
-    // console.log(typeofinput.value);
+
     // Make this is a function?
     if (!isNum) {
       errorMsg.innerHTML = errorMsgsList[1];
@@ -106,12 +96,10 @@ function calculateTip(num) {
 
   if (billValue && peopleValue && currentTip) {
     let tip = num;
+
     let tipAmt;
-    if (num < 1) {
-      tipAmt = billValue * tip;
-    } else {
-      tipAmt = tip;
-    }
+
+    num < 1 ? (tipAmt = billValue * tip) : (tipAmt = tip);
 
     let totalAmt = billValue + tipAmt;
 
@@ -150,66 +138,3 @@ function resetApp() {
   resetTipBtns();
   resetResults();
 }
-
-/******************
-EVENT LISTENERS 
-*******************/
-
-// Validate input field for bill
-// billInput.addEventListener('change', function () {
-//   billValue = Number(this.value);
-//   let isNum = floatRegex.test(billValue);
-
-//   !isNum
-//     ? (billErrorMsg.innerHTML = `Please enter a valid number.`)
-//     : billValue === 0
-//     ? (billErrorMsg.innerHTML = `Cannot be zero`)
-//     : (billErrorMsg.innerHTML = ``);
-
-//   calcShowResults(currentTip);
-// });
-
-// Validate input field for people
-// peopleInput.addEventListener('change', function () {
-//   peopleValue = Number(this.value);
-//   let isNum = intRegex.test(peopleValue);
-
-//   !isNum
-//     ? (peopleErrorMsg.innerHTML = `Please enter a valid number.`)
-//     : peopleValue === 0
-//     ? (peopleErrorMsg.innerHTML = `Cannot be a zero`)
-//     : (peopleErrorMsg.innerHTML = ``);
-
-//   calcShowResults(currentTip);
-// });
-
-// tipPercents.forEach((tip, i) => {
-//   tip.addEventListener('click', function () {
-//     let isInputEmpty = verifyNonEmptyInput();
-//     currentTip = tips[i];
-//     tip.style.color = 'tomato';
-
-//     isInputEmpty
-//       ? (tipErrorMsg.innerHTML = `Bill or Number of People cannot be empty`)
-//       : calcShowResults(tips[i]);
-
-//     customTipInput.value = ``;
-//   });
-// });
-
-// customTipInput.addEventListener('change', function () {
-//   customTipValue = Number(this.value);
-//   let isNum = intRegex.test(customTipValue);
-
-//   let isInputEmpty = verifyNonEmptyInput();
-//   currentTip = customTipValue;
-//   customTipInput.style.color = 'tomato';
-
-//   !isNum
-//     ? (tipErrorMsg.innerHTML = `Please enter a valid number.`)
-//     : isInputEmpty
-//     ? (tipErrorMsg.innerHTML = `Bill or Number of People cannot be empty`)
-//     : calcShowResults(customTipValue);
-// });
-
-// resetBtn.addEventListener('click', reset);
