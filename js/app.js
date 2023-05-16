18c9adeae3647283bb51abfe90252726a274c1ca
tinuola <24995224+tinuola@users.noreply.github.com>
2023-05-15 19:06:54 -0700

    Refactoring contd

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ let currentTip;
 
 const billInput = document.querySelector('#bill-input');
 const peopleInput = document.querySelector('#people-input');
+const customTipInput = document.querySelector('#custom-tip');
 
 const tipPercents = document.querySelectorAll('.tip-percent');
-const customTipInput = document.querySelector('#custom-tip');
 
 const errorMsgs = document.querySelectorAll('.display-error p');
 
@@ -16,13 +16,14 @@ const totalAmtDisplay = document.querySelector('#total-amount-display');
 const resetBtn = document.querySelector('#reset-btn');
 
 const floatRegex = /^[+]?(?=.)(?:\d+,)*\d*(?:\.\d+)?$/;
-const intRegex = /^\d*$/;
+// const intRegex = /^\d*$/;
+const intRegex = /^[0-9]*$/;
 
 const errorMsgsList = {
-  1: 'Enter a valid number',
-  2: "Can't be zero",
-  3: 'Must be a whole number',
-  4: 'Must be more than $1.00',
+  1: 'Enter a valid and positive number',
+  2: "Can't be zero or empty",
+  3: 'Enter a valid, positive, whole number',
+  4: 'Must be at least $1.00',
 };
 
 /*
@@ -31,29 +32,97 @@ EVENT LISTENERS
 ******************
 */
 
-document.addEventListener('input', function (e) {
-  let input = e.target;
-  let inputValue = Number(input.value);
-  let inputIndex = input.getAttribute('data-input');
+billInput.addEventListener('input', function (e) {
+  let value = Number(this.value);
+  let position = this.getAttribute('data-input');
 
-  if (input.matches('.input-num-field')) {
-    validateInput(input, inputValue, inputIndex);
+  // validateInput(this, value, position);
+
+  let isNum = floatRegex.test(value);
+
+  // validateInput( isNum, value, position )
+
+  if (!isNum) {
+    showErrorMsg(this, position, 1);
+  } else if (value > 0 && value < 1) {
+    showErrorMsg(this, position, 4);
+  } else if (Number(value) === 0) {
+    showErrorMsg(this, position, 2);
+  } else if (value > 1) {
+    removeErrorMsg(this, position);
+    billValue = value;
+    calculateTip();
+    // console.log(billValue);
   }
 });
 
-document.addEventListener('change', function (e) {
-  let input = e.target;
-  let inputValue = Number(input.value);
-  let inputIndex = input.getAttribute('data-input');
+customTipInput.addEventListener('input', function (e) {
+  let value = Number(this.value);
+  let position = this.getAttribute('data-input');
 
-  if (input.matches('.input-num-field')) {
-    if (inputIndex === '1' || inputIndex === '2') {
-      validateMoreThanDollar(input, inputValue, inputIndex);
-    } else {
-      validateWholeNum(input, inputValue, inputIndex);
-    }
+  let isNum = floatRegex.test(value);
+
+  if (!isNum) {
+    showErrorMsg(this, position, 1);
+    currentTip = 0;
+    resetResults();
+  } else if (value > 0 && value < 1) {
+    showErrorMsg(this, position, 4);
+    currentTip = 0;
+    resetResults();
+  } else if (Number(value) === 0) {
+    showErrorMsg(this, position, 2);
+    currentTip = 0;
+    resetResults();
+  } else if (value > 1) {
+    removeErrorMsg(this, position);
+    currentTip = value;
+    calculateTip();
+    // console.log(currentTip);
   }
 });
+
+peopleInput.addEventListener('input', function (e) {
+  let value = Number(this.value);
+  let position = this.getAttribute('data-input');
+
+  let isNum = intRegex.test(value);
+
+  if (!isNum) {
+    showErrorMsg(this, position, 3);
+  } else if (Number(value) === 0) {
+    showErrorMsg(this, position, 2);
+  } else if (value >= 1) {
+    removeErrorMsg(this, position);
+    peopleValue = value;
+    calculateTip();
+    // console.log(peopleValue);
+  }
+});
+
+// document.addEventListener('input', function (e) {
+//   let input = e.target;
+//   let inputValue = Number(input.value);
+//   let inputIndex = input.getAttribute('data-input');
+
+//   if (input.matches('.input-num-field')) {
+//     validateInput(input, inputValue, inputIndex);
+//   }
+// });
+
+// document.addEventListener('change', function (e) {
+//   let input = e.target;
+//   let inputValue = Number(input.value);
+//   let inputIndex = input.getAttribute('data-input');
+
+//   if (input.matches('.input-num-field')) {
+//     if (inputIndex === '1' || inputIndex === '2') {
+//       validateMoreThanDollar(input, inputValue, inputIndex);
+//     } else {
+//       validateWholeNum(input, inputValue, inputIndex);
+//     }
+//   }
+// });
 
 // Designated "click" events
 document.addEventListener('click', function (e) {
@@ -63,7 +132,7 @@ document.addEventListener('click', function (e) {
     customTipInput.value = '';
     let tipValue = elem.getAttribute('data-tip-amount');
     currentTip = Number(tipValue / 100);
-    calculateTip(currentTip);
+    calculateTip();
   }
 
   if (elem.matches('#custom-tip')) {
@@ -81,49 +150,48 @@ FUNCTIONS
 ******************
 */
 
-function validateInput(inputElem, inputVal, inputIdx) {
-  let isNum = floatRegex.test(inputVal);
+// function validateInput(inputElem, inputVal, inputIdx) {
+//   let isNum = floatRegex.test(inputVal);
 
-  if (!isNum) {
-    showErrorMsg(inputElem, inputIdx, 1);
-  } else if (inputElem.value === '0') {
-    showErrorMsg(inputElem, inputIdx, 2);
-  } else {
-    removeErrorMsg(inputElem, inputIdx);
-    switch (inputIdx) {
-      case '1':
-        billValue = inputVal;
-        break;
-      case '2':
-        currentTip = inputVal;
-        break;
-      case '3':
-        peopleValue = inputVal;
-        break;
-    }
-    calculateTip(currentTip);
-  }
-}
+//   if (!isNum) {
+//     showErrorMsg(inputElem, inputIdx, 1);
+//   } else {
+//     removeErrorMsg(inputElem, inputIdx);
+// switch (inputIdx) {
+//   case '1':
+//     billValue = inputVal;
+//     break;
+//   case '2':
+//     currentTip = inputVal;
+//     break;
+//   case '3':
+//     peopleValue = inputVal;
+//     break;
+// }
+// calculateTip(currentTip);
+//   }
+// }
 
-function validateWholeNum(inputElem, inputVal, inputIdx) {
-  let isWhole = intRegex.test(inputVal);
-  if (!isWhole) {
-    showErrorMsg(inputElem, inputIdx, 3);
-    peopleValue = 0;
-  } else {
-    removeErrorMsg(inputElem, inputIdx);
-  }
-}
+// function validateWholeNum(inputElem, inputVal, inputIdx) {
+//   let isWhole = intRegex.test(inputVal);
+//   if (!isWhole || inputVal === NaN) {
+//     console.log(inputVal);
+//     showErrorMsg(inputElem, inputIdx, 3);
+//     peopleValue = 0;
+//   } else {
+//     removeErrorMsg(inputElem, inputIdx);
+//   }
+// }
 
-function validateMoreThanDollar(inputElem, inputVal, inputIdx) {
-  if (inputVal < 1) {
-    showErrorMsg(inputElem, inputIdx, 4);
-    billValue = 0;
-    currentTip = 0;
-  } else {
-    removeErrorMsg(inputElem, inputIdx);
-  }
-}
+// function validateMoreThanDollar(inputElem, inputVal, inputIdx) {
+//   if (inputVal < 1) {
+//     showErrorMsg(inputElem, inputIdx, 4);
+//     billValue = 0;
+//     currentTip = 0;
+//   } else {
+//     removeErrorMsg(inputElem, inputIdx);
+//   }
+// }
 
 function showErrorMsg(inputElem, inputFieldNum, msgNum) {
   // Subtract 1 to match field position to errorMsg array indices
@@ -134,18 +202,13 @@ function showErrorMsg(inputElem, inputFieldNum, msgNum) {
 
 function removeErrorMsg(inputElem, inputFieldNum) {
   // Subtract 1 to match field position to errorMsg array indices
-  console.log(errorMsgs[inputFieldNum - 1]);
   errorMsgs[inputFieldNum - 1].classList.add('hide');
   inputElem.classList.remove('error');
 }
 
-function calculateTip(num) {
+function calculateTip() {
   if (billValue && peopleValue && currentTip) {
-    let tip = num;
-
-    let tipAmt;
-
-    num < 1 ? (tipAmt = billValue * tip) : (tipAmt = tip);
+    currentTip < 1 ? (tipAmt = billValue * currentTip) : (tipAmt = currentTip);
 
     let totalAmt = billValue + tipAmt;
 
@@ -155,6 +218,8 @@ function calculateTip(num) {
 
     resetBtn.removeAttribute('disabled');
     resetBtn.classList.add('active');
+  } else {
+    resetResults();
   }
 }
 
